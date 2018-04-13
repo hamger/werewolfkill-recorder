e87eafbcc817e76ca768ac8af88d1516f1e87d10
Hanger <2313303800@qq.com>
2018-04-13 11:56:03 +0800

    项目初始化

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import './App.less';
+import 'hg-parapicker/dist/picker.min.css';
+import ParaPicker from 'hg-parapicker';
+import Begin from '~/begin/begin.js';
+
+// var players = [1,2,3,4,5,6,7,8,9,10,11,12];
+
+var data = [
+    ['平民', '狼人', '预言家', '女巫', '猎人', '白痴'],
+    ['存活', '死亡', '吃刀', '票出', '吃毒', '中枪']
+];
+
+class App extends Component {
+  	constructor(props) {
+	    super(props);
+	    this.state = {
+	    	players: [
+    			{
+    				identity: '平民',
+    				state: '存活',
+    			},{
+    				identity: '平民2',
+    				state: '存活',
+    			},{
+    				identity: '平民3',
+    				state: '存活',
+    			},{
+    				identity: '平民4',
+    				state: '存活',
+    			},
+    		]
+	    };
+  	}
+
+  	componentDidMount () {
+  		let len = this.state.players.length;
+  		for (var i = 0; i < len; i++) {
+	  		new ParaPicker({
+	            inputId: `btn${i}`,
+	            title: `${i}号玩家`,
+	            data: data,
+	            success: (arr) => {
+	       //          this.setState({
+				    //   date: new Date()
+				    // });
+	            }
+	        });
+  		}
+  	}
+
+  	render() {
+    	return (
+	      	<div className="App">
+	      		<Begin />	
+	        	<ul className="number-box">  
+	          		{
+	          			this.state.players.map((val, index) => {
+	          				return (
+	          		 			<li id={`btn${index}`} key={index}>
+	          		 				{val.identity} <br /> {val.state}
+          		 				</li>
+          					)
+	          			})
+          		 	}  
+	        	</ul>
+	      	</div>
+    	);
+  	}
+}
+
+export default App;
---
+++ src/components/begin/begin.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './begin.less';
+
+var options = ['平民', '狼人', '预言家', '女巫', '猎人', '白痴'];
+
+class Begin extends Component {
+  	constructor(props) {
+	    super(props);
+	    this.state = {};
+  	}
+
+  	render() {
+    	return (
+      	<div className="begin">
+          <h3>请选择本局配置</h3> 
+        	<div className="options-box">  
+          		{
+          			options.map((val, index) => {
+          				return (
+                    <span>{val}</span>
+        					)
+          			})
+        		 	}  
+        	</div>
+      	</div>
+    	);
+  	}
+}
+
+export default Begin;
